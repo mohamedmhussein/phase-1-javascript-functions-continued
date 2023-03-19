5819579b857bd157bd026ecad4cb6cdc2d1b4187
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-03-18 21:31:34 -0400

    complicated features of functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = "go to the office"){
+                return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visual="*"){
+    function innerFunction(input = "special"){
+        return `You are ${visual}${input}${visual}!`
+    }
+    return innerFunction
+}
+console.log(wrapAdjective("%")("a dedicated programmer"))
